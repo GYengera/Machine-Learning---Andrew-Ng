4bc42cf9f4720708848ac186d48815a7efebffa0
GYengera <g.yengera@gmail.com>
2016-03-09 03:19:33 +0530

    Coursera - Machine Learning (Andrew Ng)
    My solutions to Andrew Ng's course on Machine Learning

---
+++ SVM/mlclass-ex6/dataset3Params.m
@@ -0,0 +1,53 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+C =1 ;
+sigma = 0.1;
+
+%m = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+%sig= [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+%a=zeros(8,8);
+% You need to return the following variables correctly.
+%for i=1:length(m)
+ %   for j=1:length(sig) 
+  %      model = svmTrain(X, y, m(i), @(x1, x2) gaussianKernel(x1, x2, sig(j)));
+  %      predictions = svmPredict (model, Xval);
+  %      error = mean(double(predictions~= yval));
+  %      disp (m(i), sig(j), error);
+  %  end
+%end
+%b=min((min(a)),1);
+%for i = 1:8
+%    for j=1:8
+%        if a(i,j)== b
+%C = m(i);
+%sigma = sig(j);
+%        end
+%    end
+%end
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+
+
+
+
+
+
+% =========================================================================
+
+end
